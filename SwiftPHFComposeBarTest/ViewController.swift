8bd0aea49050ff56f7b5be9e27476f60df5f69b6
James Bebbington <james.bebbington@gmail.com>
2015-07-06 13:19:37 +0100

    Attempt to make PHFComposeBarView first responder

--- SwiftPHFComposeBarTest/ViewController.swift
+++ SwiftPHFComposeBarTest/ViewController.swift
@@ -32,6 +32,10 @@ class ViewController: UIViewController, PHFComposeBarViewDelegate {
         self.composeBar.removeFromSuperview()
     }
 
+    override func viewDidAppear(animated: Bool) {
+        self.composeBar.becomeFirstResponder()
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
