349c19c563afdaab1ad806074c13e621ead32b57
James Bebbington <james.bebbington@gmail.com>
2015-07-06 10:55:15 +0100

    Initial Commit

---
+++ SwiftPHFComposeBarTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPHFComposeBarTest
+//
+//  Created by James Bebbington on 06/07/2015.
+//  Copyright © 2015 Liveminds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
